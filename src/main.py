04c2116e2090f20c1588390233f8c6f22d7e0f47
oriol <ogomez.s92@gmail.com>
2022-05-22 19:57:20 +0200

    feat: Create rudimentary UI

--- src/main.py
+++ src/main.py
@@ -2,3 +2,10 @@ from managers.ui_manager import UIManager
 from managers.data_manager import DataManager
 
 dataManager = DataManager()
+uiManager = UIManager()
+
+uiManager.createUI()
+print("hi")
+if not dataManager.existsCommandsFile():
+    print("test")
+    uiManager.showAlert("fuck you", "title")
--- src/managers/ui_manager.py
+++ src/managers/ui_manager.py
@@ -1,8 +1,22 @@
 import wx
 
+
 class UIManager:
     def createUI(self):
-        app = wx.App()
-        frame = wx.Frame(None, title='Launchtype')
-        frame.Show()
-        app.MainLoop()
+        self.app = wx.App(False)
+        self.frame = wx.Frame(None, -1, "Main Window", size=(300, 200))
+        self.panel = wx.Panel(self.frame, -1)
+
+        self.button = wx.Button(self.panel, -1, "Add Command", pos=(100, 20))
+        self.app.Bind(wx.EVT_BUTTON, self.addButtonClicked, self.button)
+
+    def initialize_ui(self):
+        self.app.mainLoop()
+        
+    def showAlert(self, title, text):
+        dlg = wx.MessageDialog(None, title, text, wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def addButtonClicked(self):
+        print("test")
