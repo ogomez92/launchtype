b5f2a749eb7fedaaec724ee5836c07be9c934095
ogomez-at-wiris <oriol.gomez@wiris.com>
2022-07-06 06:23:04 +0200

    chore: remove consolelogs

--- src/helpers/sound_player.py
+++ src/helpers/sound_player.py
@@ -4,5 +4,4 @@ class SoundPlayer:
     @staticmethod
     def play(filename):
         assembled_filename = f"sounds/{filename}.wav"
-        print(assembled_filename)
         winsound.PlaySound(assembled_filename, winsound.SND_ASYNC)
