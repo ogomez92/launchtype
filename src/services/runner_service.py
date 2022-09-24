4c7e681b572f8992e4c03a99626f644ef727f63e
oriol <ogomez.s92@gmail.com>
2022-09-24 17:05:19 +0200

    fix: Fix commands with empty command line arguments

--- src/services/runner_service.py
+++ src/services/runner_service.py
@@ -4,11 +4,16 @@ from helpers.sound_player import SoundPlayer
 
 
 def run_command(path, args):
-    # separate args by comma
+    comma_separated_args = None
+    print('args', args)
+    if args is not '':
+        # separate args by comma
 
-    comma_separated_args = [arg.strip() for arg in args.split(',')]
+        comma_separated_args = [arg.strip() for arg in args.split(',')]
+
     cwd = os.path.dirname(path)
     SoundPlayer.play("run")
     command_to_run = [path]
-    command_to_run.extend(comma_separated_args)
+    if comma_separated_args is not None:
+        command_to_run.extend(comma_separated_args)
     subprocess.Popen(command_to_run, cwd=cwd)
