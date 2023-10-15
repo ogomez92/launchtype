8d9168fa451d9e620e1392c94655fdcbb6503edb
Oriol <ogomez.s92@gmail.com>
2023-10-15 08:41:58 +0200

    fix: attempt to fix an annoying translations issue and fallback locale

--- src/language_handler.py
+++ src/language_handler.py
@@ -3,6 +3,10 @@ import locale
 
 def initialize():
     current_locale, encoding = locale.getlocale()
+
+    if not current_locale:
+        current_locale = "en"
+        
     language_code = current_locale.split("_")[0]
     translations = gettext.translation('launchtype', localedir='locale', languages=[language_code], fallback = True)
     translations.install()
